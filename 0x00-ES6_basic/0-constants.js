130a85e251819c60f7c4f126f4e73db5fe1df5d8
ABeldams <87949987+ABeldams@users.noreply.github.com>
2023-07-25 17:22:11 +0100

    Update 0-constants.js

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,11 +1,11 @@
 export function taskFirst(){
- var task = 'I prefer const when I can.';
+ const task = 'I prefer const when I can.';
 }
 export function getLast(){
   return ' is okay';
 }
 export function taskNext(){
- var combination = 'But sometimes let';
+ let combination = 'But sometimes let';
   combination +=getLast();
 
   return combination;
