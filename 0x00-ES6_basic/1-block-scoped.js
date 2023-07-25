1db14abd893277bd194d8bc2486b208db67f8baf
ABeldams <87949987+ABeldams@users.noreply.github.com>
2023-07-25 17:37:09 +0100

    Update 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,9 @@
 export default function taskBlock(task, task2) {
-  var task = false;
-  var task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (task || task2) {
-    var task = true;
-    var task2 = false;
+  return [task, task2];  
   }
 
   return [task, task2];
